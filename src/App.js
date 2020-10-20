4bc8d36bbdf92b604d58112b4a915356b05431ee
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-10-20 15:37:30 -0700

    bringing amplify export to our main app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-
 import "./App.css";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
   return <div className="App">Hello I'm clean!!</div>;
