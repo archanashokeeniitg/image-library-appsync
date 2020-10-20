244bfa0426d826cd35be1fa3ec6f2f22d07ccb90
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-10-20 16:40:41 -0700

    modifying the signup-signin pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+
+//amplify exports
 import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import awsExports from "./aws-exports";
+
 Amplify.configure(awsExports);
 
 function App() {
-  return <div className="App">Hello I'm clean!!</div>;
+  useEffect(() => {
+    document.body.style.background =
+      "linear-gradient(45deg, #51547e 30%, #0a3f74 90%)";
+  }, []);
+  return (
+    <AmplifyAuthenticator style={{ textAlign: "center" }}>
+      <div className="App">Hello I'm clean!!</div>
+    </AmplifyAuthenticator>
+  );
 }
 
 export default App;
