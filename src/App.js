f3ccc4d7074bc0b3a17021654ff61e1cd4b5d1e9
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-10-20 18:16:23 -0700

    components navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 //amplify exports
@@ -6,16 +6,27 @@ import Amplify from "aws-amplify";
 import { AmplifyAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import awsExports from "./aws-exports";
 
+//components imports
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
 Amplify.configure(awsExports);
 
 function App() {
+  const [user, setUser] = useState("");
   useEffect(() => {
     document.body.style.background =
       "linear-gradient(45deg, #51547e 30%, #0a3f74 90%)";
   }, []);
   return (
     <AmplifyAuthenticator style={{ textAlign: "center" }}>
-      <div className="App">Hello I'm clean!!</div>
+      <div className="App">
+        <row>
+          <Header />
+        </row>
+        Hello I'm clean!!
+        <Footer />
+      </div>
     </AmplifyAuthenticator>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+//components
+import Home from "../Home";
+import Upload from "../Upload";
+
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory as createHistory } from "history";
+import { AmplifySignOut } from "@aws-amplify/ui-react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+const history = createHistory();
+
+const Header = () => {
+  return (
+    <div>
+      <Router history={history}>
+        <Navbar color="dark">
+          <NavbarBrand href="/" className="text-white">
+            Image Gallery
+          </NavbarBrand>
+          <Nav>
+            <NavItem>
+              <NavLink className="text-white" href="/">
+                Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className="text-white" href="/upload">
+                upload Image
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <AmplifySignOut />
+        </Navbar>
+        <Route path="/" exact component={Home} />
+        <Route path="/upload" exact component={Upload} />
+      </Router>
+    </div>
+  );
+};
+
+export default Header;
