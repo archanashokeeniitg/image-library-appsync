d0b38f34215b725fcb7a21e436804f744dff3cb0
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-10-21 04:08:17 -0700

    ui fix for display

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card, CardImg, Row, Col } from "reactstrap";
 
 class ImageGallery extends Component {
   constructor(props) {
@@ -8,30 +9,36 @@ class ImageGallery extends Component {
     console.log("inside Imagegallery", this.props.images);
     return (
       <div className="container">
-        {this.props.images.map((image) => (
-          <div key={image.id} className="jumbotron">
-            <i
-              className="fa fa-trash col-lg-2"
-              aria-hidden="true"
-              onClick={(event) => {
-                this.props.deleteImage(image.id);
-              }}
-            ></i>
-            <img
-              className=" col-lg-8"
-              src={image.src}
-              alt="Smiley face"
-              width="300"
-            />
-
-            <i
-              className="fa fa-download col-lg-2"
-              onClick={(event) => {
-                this.props.downloadImage(image.src);
-              }}
-            ></i>
-          </div>
-        ))}
+        <Row>
+          {this.props.images.map((image) => (
+            <Col>
+              <Card key={image.id} className="  jumbotron ">
+                <div>
+                  <i
+                    className="fa fa-trash pull-left "
+                    aria-hidden="true"
+                    onClick={(event) => {
+                      this.props.deleteImage(image.id);
+                    }}
+                  ></i>
+                  <i
+                    className="fa fa-download   pull-right "
+                    onClick={(event) => {
+                      this.props.downloadImage(image.src);
+                    }}
+                  ></i>
+                  <CardImg
+                    className=""
+                    src={image.src}
+                    alt="Smiley face"
+                    width="100"
+                    height="200"
+                  />
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
       </div>
     );
   }
