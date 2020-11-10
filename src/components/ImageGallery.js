273842be4319d4172f8029f3ebfa717881dbbb15
Eric Cheng <echeng@scu.edu>
2020-11-09 18:48:29 -0800

    Add working localhost

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -3,7 +3,7 @@ import { Card, CardImg, Row, Col, CardText } from "reactstrap";
 
 import { API, graphqlOperation, label } from "aws-amplify";
 import { updatePicture } from "../graphql/mutations";
-// import awsExports from "../aws-exports";
+import awsExports from "../aws-exports";
 import "./Home.css";
 
 function ImageGallery(props) {
